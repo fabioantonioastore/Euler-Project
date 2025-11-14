30a8893b0f9927f5e5ac16e2bd5e1c1b368480ab
ninja <astore.a.fabio@gmail.com>
2025-11-13 23:03:27 -0300

    problem 9

---
+++ problem_9.py
@@ -0,0 +1,9 @@
+def find_solution() -> int:
+    for c in range(999, 1, -1):
+        for b in range(999 - c, 1, -1):
+            for a in range(b, 1, -1):
+                if a + b + c == 1000 and (a ** 2) + (b ** 2) == (c ** 2):
+                    return a * b * c
+
+
+print(find_solution())
